7fe1e8d8143c1aa7d58f6bf6500d0135693631e6
Siirabs <63456517+Siirabs@users.noreply.github.com>
2021-12-11 20:32:23 +0200

    fixes

--- server/routes/api.js
+++ server/routes/api.js
@@ -64,6 +64,7 @@ router.get("/snippet", (req, res, next) => {
         snippets = result;
         db.close();
       });
+    res.status(200).send("ok");
   });
   console.log(snippets);
   return { snippets };
@@ -99,83 +100,5 @@ let idGen = () => {
     .toString(16)
     .substring(1);
 };
-
-router.get("/login", (req, res, next) => {
-  res.render("login");
-});
-
-router.post("/login", upload.none(), (req, res, next) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (err) throw err;
-    if (!user) {
-      return res.status(403).json({ message: "Invalid credentials" });
-    } else {
-      bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
-        if (err) throw err;
-        if (isMatch) {
-          const jwtPayload = {
-            id: user._id,
-            email: user.email,
-          };
-          jwt.sign(
-            jwtPayload,
-            process.env.SECRET,
-            {
-              expiresIn: 1200,
-            },
-            (err, token) => {
-              console.log(token);
-              res.json({ success: true, token });
-            }
-          );
-        } else {
-          return res.status(403).json({ message: "Invalid credentials" });
-        }
-      });
-    }
-  });
-});
-
-router.get("/register", (req, res, next) => {
-  res.render("register");
-});
-
-router.post(
-  "/register",
-  upload.none(),
-  body("email").isEmail().trim().escape(),
-  body("password").isLength({
-    min: 8,
-  }) /*.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()-_+={}[\]\|\\;:"<>,.\/\?]).{8,}$/)*/,
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(403).json({ message: "Password is not strong enough" });
-    }
-    console.log(req.body);
-    User.findOne({ email: req.body.email }, (err, user) => {
-      if (err) throw err;
-      if (user) {
-        return res.status(403).json({ message: "Email already in use" });
-      } else {
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(req.body.password, salt, (err, hash) => {
-            if (err) throw err;
-            User.create(
-              {
-                email: req.body.email,
-                password: hash,
-              },
-              (err, ok) => {
-                if (err) throw err;
-                return res.send({});
-              }
-            );
-          });
-        });
-      }
-    });
-  }
-);
 
 module.exports = router;
